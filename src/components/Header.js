ef0512b7199aeab3ab26deb073afc0d0de96523e
kalvin104 <jpwickham@hotmail.co.uk>
2023-06-07 16:08:35 +0100

    new ui again

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,17 +11,25 @@ export default function Header({openHome, openCollection, openAddGuitar, openCol
 
   const selectHome = {
     backgroundColor: home ? "#F0F0F0" : "",
-    color: home ? "black" : "white"
+    color: home ? "black" : "white",
+    paddingLeft: "25px",
+    paddingRight: "25px"
   }
   const selectCollection = {
     backgroundColor: openColl ? "#F0F0F0" : "",
     color: openColl ? "black" : "white",
-    paddingTop: "30px", paddingLeft: "5px", height: "51px", width: "85px"
+
+    paddingLeft: "25px",
+    paddingRight: "25px",
+    paddingTop: "50px"
   }
   const selectAddGuitar = {
     backgroundColor: showAdd ? "#F0F0F0" : "",
     color: showAdd ? "black" : "white",
-    paddingTop: "30px", paddingLeft: "5px", height: "51px", width: "85px"
+    
+    paddingLeft: "25px",
+    paddingRight: "25px",
+    paddingTop: "50px"
   }
 
 
@@ -30,13 +38,14 @@ export default function Header({openHome, openCollection, openAddGuitar, openCol
     <div style={{paddingRight: "100px", paddingLeft: "50px", display: "flex", backgroundColor: "#1c1c1c", color: "white"}}>
         <h1
         style={selectHome}
+        className="tab"
         id='home-icon'
         onClick={() => openHome()}
         >MyGuitarHub</h1>
 
-        <div style={{marginLeft: "50px", display: "flex", gap: "30px"}}>
-          <div id='addGuitar' style={selectAddGuitar} onClick={() => openAddGuitar()}>New Guitar</div>
-          <div id='addGuitar' style={selectCollection} onClick={() => openCollection()}>Collection</div>
+        <div style={{display: "flex"}}>
+          <div className="tab" id='addGuitar' style={selectAddGuitar} onClick={() => openAddGuitar()}>New Guitar</div>
+          <div className="tab" id='addGuitar' style={selectCollection} onClick={() => openCollection()}>Collection</div>
         </div>
        
     </div>
